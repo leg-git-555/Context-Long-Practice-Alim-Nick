da1b96cb38bcbd838a7edef3db9e131269489612
Ciara <ciarafumar@gmail.com>
2022-07-27 14:46:36 -0700

    05

--- problems/05-bind-to-an-arg.js
+++ problems/05-bind-to-an-arg.js
@@ -1,6 +1,27 @@
 function bindToAnArg(func, arg) {
-  // Your code here
+  return func.bind(bindToAnArg, arg)
+  // console.log(func.bind(arg))
+  // return (func.bind())
 }
+
+function add(num1, num2) {
+  return num1 + num2;
+}
+
+const addTwo = bindToAnArg(add, 2);
+const addThree = bindToAnArg(add, 3);
+
+const twoPlusSix = addTwo(6);
+const twoPlusSeven = addTwo(7);
+const threePlusSeven = addThree(7);
+const threePlusEight = addThree(8);
+console.log({
+  twoPlusSix,     // => 8
+  twoPlusSeven,   // => 9
+  threePlusSeven, // => 10
+  threePlusEight  // => 11
+});
+
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
 module.exports = bindToAnArg;
