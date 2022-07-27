310a8114f76a6fa64432e9dfedf9f9b71d6f6777
Nick Leger <nick.leger2314@gmail.com>
2022-07-27 16:43:30 -0400

    01

--- problems/01-car-drive.js
+++ problems/01-car-drive.js
@@ -1,4 +1,26 @@
 // Your code here
+class Car {
+	constructor(speed = 0) {
+		this.speed = speed
+	}
+
+	drive(newSpeed){
+		this.speed += newSpeed;
+		console.log(newSpeed);
+		return this.speed;
+	}
+
+
+}
+
+let car = new Car();
+// car.drive(0);     // => returns 0
+// console.log(car); // => { speed: 0 }
+
+console.log(car);
+car.drive(10);    // => returns 10
+console.log(car); // => { speed: 10, }
+
 
 /*****************************************************************************/
 /***************** DO NOT MODIFY ANYTHING UNDER THIS LINE ********************/
